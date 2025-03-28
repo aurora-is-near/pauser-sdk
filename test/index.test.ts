e5b7431ad48864868540444625ea9909f040de4f
Diego Figueroa <diego.figueroa@aurora.dev>
2025-03-28 17:34:05 -0400

    test: use consistent params for input validation tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,7 +3,7 @@ import { pause, PauseSdkError, unpause } from '../src';
 describe('pause', () => {
   it('should throw an INVALID_PARAMETERS error for pause with invalid input', async () => {
     await expect(
-      pause({ networkId: 'ethereum', chainId: '', accountId: '' }),
+      pause({ networkId: 'ethereum', chainId: 1, accountId: '' }),
     ).rejects.toThrow(PauseSdkError);
   });
 });
@@ -11,7 +11,7 @@ describe('pause', () => {
 describe('unpause', () => {
   it('should throw an INVALID_PARAMETERS error for unpause with invalid input', async () => {
     await expect(
-      unpause({ networkId: 'network', chainId: 1, accountId: '' }),
+      unpause({ networkId: 'ethereum', chainId: 1, accountId: '' }),
     ).rejects.toThrow(PauseSdkError);
   });
 });
